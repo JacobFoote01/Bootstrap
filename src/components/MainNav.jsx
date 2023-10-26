0eab6e6aa845b4ef309dbf2c32c701cb0138a81e
Jacob Foote <jacobjfoote01@gmail.com>
2023-10-26 12:08:43 -0600

    commit

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,21 +1,28 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
+
 export default function MainNav({ brand, leftLinks, rightLinks }) {
   return (
-    <nav>
-      <a href="/">{brand}</a>
-      <ul>
-        {leftLinks.map(({ url, text }) => (
-          <li key={text}>
-            <a href={url}>{text}</a>
-          </li>
+    <Navbar expand='lg' className="bg-success navbar-dark">
+      <Container fluid>
+      <Navbar.Brand href="#Home">{brand}</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar">
+      <Nav className="me-auto">
+        {leftLinks.map(({ url, text }, index) => (
+          <Nav.Link key={index} href={url}>
+            {text}
+          </Nav.Link>
         ))}
-      </ul>
-      <ul>
-        {rightLinks.map(({ url, text }) => (
-          <li key={text}>
-            <a href={url}>{text}</a>
-          </li>
+      </Nav>
+      <Nav>
+        {rightLinks.map(({ url, text }, index) => (
+          <Nav.Link key={index} href={url}>
+            {text}
+          </Nav.Link>
         ))}
-      </ul>
-    </nav>
+      </Nav>
+    </Navbar.Collapse>
+    </Container>
+    </Navbar>
   );
 }
